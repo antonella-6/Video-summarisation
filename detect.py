8dab470deefde2f8171b2a9c347a61e0a073b2be
antonella-6 <68552106+antonella-6@users.noreply.github.com>
2020-10-21 13:18:09 +0200

    Create detect.py

---
+++ detect.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import subprocess
+from scenedetect import VideoManager
+from scenedetect import SceneManager
+from scenedetect.detectors import ContentDetector
+
+folders = os.listdir("./luca")
+threshold = int(sys.argv[1]) if len(sys.argv) > 1 else 35
+
+for folder in folders:
+    folder_path = f"{os.getcwd()}/luca/{folder}"
+    video_file_path = f"{folder_path}/video.mp4"
+    print(video_file_path);
+    command = f"scenedetect --input {video_file_path} --output {folder_path} detect-content --threshold {threshold} list-scenes save-images"
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)
